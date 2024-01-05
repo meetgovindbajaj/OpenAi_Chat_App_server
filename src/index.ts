d8ece88df86ba6d73177f645975155e0b8a40e41
Govind Bajaj <meetgovindbajaj@gmail.com>
2024-01-05 16:50:13 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,19 @@ const PORT: string | number = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: "*",
+    origin: "https://open-ai-chat-app-eight.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 ); //used to allow special domains to send requests to server
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//     crossOriginResourcePolicy: false,
-//     crossOriginEmbedderPolicy: false,
-//   })
-// );
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    referrerPolicy: { policy: "no-referrer" },
+  })
+);
 app.use(morgan("dev")); //logs url requests
 app.use(express.json()); //parses json data sent from client
 app.use(cookieParser(process.env.COOKIE_SECRET)); //extracts cookies data from http request
