b9b977627b749b44fdc1598ef8f4e40fc6f8f8ea
Govind Bajaj <meetgovindbajaj@gmail.com>
2024-01-09 12:54:26 +0530

    updates

--- dist/index.js
+++ dist/index.js
@@ -10,26 +10,26 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 // middlewares
-// app.use(
-//   cors({
-//     origin: "https://open-ai-chat-app-eight.vercel.app",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-//   })
-// ); //used to allow special domains to send requests to server
-app.use(cors({
-    origin: "http://localhost:3000",
+app.use(
+  cors({
+    origin: "https://open-ai-chat-app-eight.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-}));
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//     crossOriginResourcePolicy: false,
-//     crossOriginEmbedderPolicy: false,
-//     referrerPolicy: { policy: "no-referrer" },
-//   })
-// );
+  })
+); //used to allow special domains to send requests to server
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+// }));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    referrerPolicy: { policy: "no-referrer" },
+  })
+);
 app.use(morgan("dev")); //logs url requests
 app.use(express.json()); //parses json data sent from client
 app.use(cookieParser(process.env.COOKIE_SECRET)); //extracts cookies data from http request
@@ -40,8 +40,8 @@ app.use("/api/v1/", route);
 // server and database connection
 console.log(PORT);
 connectDb()
-    .then(() => {
+  .then(() => {
     app.listen(PORT, () => console.log("Server Running"));
-})
-    .catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
 //# sourceMappingURL=index.js.map
